eadbc0b964e70393d16514a7152ce841eff64299
Chnum <sindre.fjermestad@fundator.no>
2017-08-28 16:12:46 +0200

    deleted node

--- PackMannjNodeJs/test.bat
+++
@@ -1,4 +0,0 @@
-start "L.A.R.S. 1" cmd /c node lars.js -n "L.A.R.S. 1"
-start "L.A.R.S. 2" cmd /c node lars.js -n "L.A.R.S. 2"
-start "L.A.R.S. 3" cmd /c node lars.js -n "L.A.R.S. 3"
-start "L.A.R.S. 4" cmd /c node lars.js -n "L.A.R.S. 4"
