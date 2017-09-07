109fec03ed56ab6ab1be71de2110b16b65cfa973
Sindre Fjermestad <sindre.fjermestad@fundator.no>
2017-09-07 17:44:48 +0200

    removed stupid entrypoint and bf fix

--- entrypoint_NAME.py
+++
@@ -1,8 +0,0 @@
-from util.Client import Client
-if __name__ == '__main__':
-    bot_name = b"NAME bbb\n" # <--- bot name goes here
-    c = Client(bot_name)
-    #TODO need a while true here or something to setup and run the bot FOR ALL
-    #THE ROUNDS :O
-    c.setup_bot()
-    c.run_bot()
